1d80fe6a74465880f6d0d1b5d443dc7dd1d9e4cf
Kurmuri <49898034+kurmuri@users.noreply.github.com>
2023-08-13 23:11:56 +0530

    Update LCSRecursion.java

--- src/main/java/org/shandilya/dp/lcs/LCSRecursion.java
+++ src/main/java/org/shandilya/dp/lcs/LCSRecursion.java
@@ -1,1 +1,21 @@
+package lcs;
 
+public class LCSRecursion {
+    public static void main(String[] args) {
+        String a = "ab";
+        String b = "ab";
+
+        System.out.println(lcs(a, b, a.length(), b.length()));
+    }
+
+    private static int lcs(String a, String b, int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+
+        if (a.charAt(m-1) == b.charAt(n-1)) {
+            return 1 + lcs(a, b, m-1, n-1);
+        } else {
+            return Math.max(lcs(a, b, m-1, n), lcs(a, b, m, n-1));
+        }
+    }
+}
